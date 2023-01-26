147a670c71a7a40fdb31ea08e35b6017c05a3fa9
vmontich <vmontich@gmail.com>
2023-01-26 17:32:05 -0300

    Saving a new Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -82,6 +82,7 @@ public class DepartmentListController implements Initializable {
 			Pane pane = loader.load();
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDeparmentService(new DepartmentService());
 			controller.updateFormData();
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter Department Data");
